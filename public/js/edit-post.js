258b15cc0b9bb57b55ddfd1862ba3f611ef8ead0
kaivon tolooee <kaivont@gmail.com>
2021-08-09 09:23:03 -0400

    changes to try to fix the edit post

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -6,8 +6,8 @@ async function editFormHandler(event) {
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-  
-    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const title ="new-note"
+    // const title = document.querySelector('input[name="post-title"]').value.trim();
     const content = document.querySelector('textarea[name="content"]').value;
   
     // Get post title and post text from the form
@@ -24,6 +24,7 @@ async function editFormHandler(event) {
   
     // if the edit action is successful, redirect to the dashboard page. Else display the error
     if (response.ok) {
+      alert('hit')
       document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
@@ -31,5 +32,5 @@ async function editFormHandler(event) {
   }
   
   document
-    .querySelector('.edit-post-form')
-    .addEventListener('submit', editFormHandler);
+    .querySelector('#edit-post-button')
+    .addEventListener('click', editFormHandler);
